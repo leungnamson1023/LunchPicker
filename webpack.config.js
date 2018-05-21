58ea35fd0b254758bb199ff7145ab664db6cc65d
Namson Leung <leungnamson@gmail.com>
2018-05-20 22:44:43 -0700

    init setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+const APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+};
+
+module.exports = config;
